bc67a7ab681a952f9377f5f2c8a041821ae881c2
nishu-rumsan <nishu.shrestha@rumsan.net>
2025-08-20 11:47:01 +0545

    Updated Gigablocks Documents

--- sidebars.js
+++ sidebars.js
@@ -1,24 +1,12 @@
 module.exports = {
   tutorialSidebar: {
-    Introduction: ["intro", "tech-stack-overview", "architecture-overview"],
-    "Open Source": ["license", "contribution-guidelines", "code-of-conduct"],
-    "Developer Guide": [
-      "getting-started-developer",
-      "software-requirements-specifications",
-    ],
-    Learn: ["community-feedback", "bounty-information"],
-    Collaborate: [
-      {
-        type: "link",
-        label: "GitHub",
-        href: "https://github.com/giga-nft2-0",
-      },
-      {
-        type: "link",
-        label: "Twitter",
-        href: "https://twitter.com/Gigaglobal",
-      },
-    ],
+    "Introduction": ["intro", "tech-stack-overview", "architecture-overview"],
+    "Getting Started": ["prerequisites", "run-locally"],
+    "Development Guidelines": ["development-guide","project"],
+    "Service Setup": ["contract", "subgraph", "giga-id"],
+    "Deployment": ["deployment"],
+    "Trouble Shooting": ["trouble-shooting"],
+    "Community": ["contribution-guidelines", "code-of-conduct"],
   },
   apiSidebar: [],
 };
